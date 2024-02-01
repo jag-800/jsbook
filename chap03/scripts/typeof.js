db9db37682f8450310a89c6833ea580654d8708a
joyun <joyunji1119@gmail.com>
2024-02-01 15:29:24 +0900

    finish/その他の演算子

---
+++ chap03/scripts/typeof.js
@@ -0,0 +1,19 @@
+let num = 1;
+console.log(typeof num);
+
+let str = 'こんにちは';
+console.log(typeof str);
+
+let flag = true;
+console.log(typeof flag);
+
+let ary = [1, 2, 3];
+console.log(typeof ary);
+
+let obj = { x:1, y:2 };
+console.log(typeof obj);
+
+let dat = new Date();
+console.log(typeof dat);
+
+// 参照型に対してはobjectとしか返さない
